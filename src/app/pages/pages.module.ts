bb735f4fbe305c162d7cc420109f69117eaca63d
Cristian Montecinos <cristian@pop-os.localdomain>
2021-05-06 00:04:23 -0400

    avance clase 47

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -26,8 +27,9 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [ 
     CommonModule,
+    FormsModule,
     SharedModule,
-    RouterModule
+    RouterModule,
   ]
 })
 export class PagesModule { }
