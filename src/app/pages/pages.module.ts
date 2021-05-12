cb859a08a43f7805eaa59d8c10f9a54ec5ed1ca4
Cristian Montecinos <cristian@pop-os.localdomain>
2021-05-11 23:47:48 -0400

    Seccion 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 @NgModule({
@@ -18,12 +19,15 @@ import { PagesComponent } from './pages.component';
     ProgressComponent,
     Grafica1Component,
     PagesComponent,
+    AccountSettingsComponent,
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent,
+
   ],
   imports: [ 
     CommonModule,
